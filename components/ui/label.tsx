a8c6fadabb04f61e72b0d4ee9a56b26cd232a372
يلا مصرى <bobaahmed811@gmail.com>
2025-12-24 03:31:25 +0200

    Update label.tsx

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -9,7 +9,6 @@ import { cn } from "@/lib/utils"
 const labelVariants = cva(
   "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
 )
-
 const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
@@ -18,9 +17,10 @@ const Label = React.forwardRef<
   <LabelPrimitive.Root
     ref={ref}
     className={cn(labelVariants(), className)}
-    ...props
+    {...props} 
   />
 ))
 Label.displayName = LabelPrimitive.Root.displayName
 
 export { Label }
+
